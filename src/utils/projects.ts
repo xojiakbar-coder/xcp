5d433b8d4a1a477b506e94f158dc5bd6c3466e3b
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-08-21 16:43:59 +0500

    book shelf removed

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -2,7 +2,6 @@ import { StaticImageData } from "next/image";
 import UdevsProject from "../../public/assets/images/projects/udevs.svg";
 import HouzingProject from "../../public/assets/images/projects/houzing.png";
 import GithubFinderProject from "../../public/assets/images/projects/g-finder.png";
-import BookShelfProject from "../../public/assets/images/projects/book-shelf.png";
 import AdminDashboardProject from "../../public/assets/images/projects/dashboard.png";
 import UnityDashboardProject from "../../public/assets/images/projects/unity-dash.png";
 
@@ -34,18 +33,12 @@ const projectsData: ProjectItemType[] = [
   },
   {
     id: 4,
-    title: "Book shelf",
-    img: BookShelfProject,
-    link: "https://book-shelf-reactjs.netlify.app/",
-  },
-  {
-    id: 5,
     title: "Github Finder",
     img: GithubFinderProject,
     link: "https://github-findder.netlify.app",
   },
   {
-    id: 6,
+    id: 5,
     title: "Houzing",
     img: HouzingProject,
     link: "https://github.com/xojiakbar-coder/houzing",
